6429a6cf37a461c685ca615600f193ee7e01a1ea
001ProMax <101964350+001ProMax@users.noreply.github.com>
2025-04-13 00:55:17 +0800

    test

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "rspress/config";
 
 export default defineConfig({
     root: path.join(__dirname, "docs"),
+    base: "/scripting-docs/",
     // outDir: "public",
     title: "Scripting",
     icon: "/scripting-icon.png",
