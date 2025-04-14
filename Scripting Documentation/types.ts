a6db0e0295a39a602779ff14a1ee44731b7ea814
001ProMax <101964350+001ProMax@users.noreply.github.com>
2025-04-14 23:58:14 +0800

    test

--- Scripting Documentation/types.ts
+++
@@ -1,9 +0,0 @@
-
-export type DocItem = {
-  title: Record<string, string>,
-  subtitle?: Record<string, string>
-  keywords?: string[]
-  example?: string
-  readme?: string
-  children?: DocItem[]
-}
