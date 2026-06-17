a4b4db0cc3eda8742699815332d53367c7cb7e1d
001ProMax <101964350+001ProMax@users.noreply.github.com>
2026-06-17 08:27:45 +0800

    fix: scripts/docs.js

--- scripts/docs.js
+++ scripts/docs.js
@@ -149,7 +149,7 @@ const processLanguages = (docItem) => {
 (async () => {
   await zip.uncompress(
     path.join(__dirname, "..", "scripting", version, "Scripting Documentation.zip"),
-    path.join(__dirname, "..", "scripting", version, "Scripting Documentation"),
+    path.join(__dirname, "..", "scripting", version),
   );
 
   const docJson = JSON.parse(readFile(path.join(resourcePath, "doc.json")));
